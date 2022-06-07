ff1f286987a7cc631d009759a03dfb146882ec3a
justgrace <ivan.koliada69@gmail.com>
2022-06-07 13:34:14 +0300

    feat: add navigation commands

---
+++ src/modules/commands/navigation.js
@@ -0,0 +1,32 @@
+import { chdir, cwd } from 'process';
+import { readdir } from 'fs/promises';
+import { error } from '../handlers/errorHandler.js';
+
+const up = () => {
+  chdir('..');
+
+  const directory = cwd();
+
+  console.log(`\x1b[34m\nYou are currently in ${directory}\x1b[0m`);
+}
+
+const cd = (path) => {
+  chdir(`${path}`);
+
+  const directory = cwd();
+
+  console.log(`\x1b[34m\nYou are currently in ${directory}\x1b[0m`);
+}
+
+const ls = async () => {
+  const pathToFolder = cwd();
+  const array = [];
+  const files = await readdir(pathToFolder, { withFileTypes: true })
+    .catch(error);
+
+  files.forEach((file) => array.push(file.name));
+
+  console.log(array);
+}
+
+export { up, cd, ls}
