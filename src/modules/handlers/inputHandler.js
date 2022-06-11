19e56665633c7bada1fded73878ea6b824d91b8a
justgrace <ivan.koliada69@gmail.com>
2022-06-11 23:17:35 +0300

    regfactor: add check commands

--- src/modules/handlers/inputHandler.js
+++ src/modules/handlers/inputHandler.js
@@ -1,8 +1,10 @@
+import { cwd } from 'process';
 import { up, cd, ls } from '../commands/navigation.js';
 import hash from '../commands/hash.js';
 import { cat, add, rn, cp, rm } from '../commands/basic.js';
 import { compress, decompress } from '../commands/archive.js';
 import { eol, cpus, homedir, architecture, username } from '../commands/system.js';
+import { error } from './errorHandler.js';
 
 const inputHandler = async (data) => {
   const input = data.split(' ');
@@ -13,54 +15,59 @@ const inputHandler = async (data) => {
 
   switch (key) {
     case 'up':
-      up();
+      if (pathToCurrent) error();
+      else up();
       break;
-  
+
     case 'cd':
-      cd(pathToCurrent);
+      if (pathToNext) error();
+      else cd(pathToCurrent);
       break;
-  
+
     case 'ls':
-      ls()
+      if (pathToCurrent) error();
+      else ls();
       break;
-    
+
     case 'hash':
-      hash(pathToCurrent)
+      hash(pathToCurrent);
       break;
-    
+
     case 'cat':
-      cat(pathToCurrent)
+      cat(pathToCurrent);
       break;
-    
+
     case 'add':
-      add(pathToCurrent)
+      add(pathToCurrent);
       break;
-    
+
     case 'rn':
-      rn(pathToCurrent, pathToNext)
+      rn(pathToCurrent, pathToNext);
       break;
-    
+
     case 'cp':
-      cp(pathToCurrent, pathToNext)
+      cp(pathToCurrent, pathToNext);
       break;
-    
+
     case 'mv':
       await cp(pathToCurrent, pathToNext);
       rm(pathToCurrent);
       break;
-    
+
     case 'rm':
-      rm(pathToCurrent, pathToNext)
+      rm(pathToCurrent, pathToNext);
       break;
-    
+
     case 'compress':
-      compress(pathToCurrent, pathToNext)
+      if (!pathToCurrent && !pathToNext) error();
+      else compress(pathToCurrent, pathToNext);
       break;
-    
+
     case 'decompress':
-      decompress(pathToCurrent, pathToNext)
+      if (!pathToCurrent && !pathToNext) error();
+      else decompress(pathToCurrent, pathToNext);
       break;
-    
+
     case 'os':
       if (flag === '--EOL') eol();
       else if (flag === '--cpus') cpus();
@@ -69,11 +76,14 @@ const inputHandler = async (data) => {
       else if (flag === '--architecture') architecture();
       else console.log('\x1b[31mInvalid input, you should enter another flag\x1b[0m');
       break;
-    
+
     default:
       console.log('\x1b[31mInvalid input, you should enter another command\x1b[0m');
       break;
   }
-}
+  const directory = cwd();
+
+  console.log(`\x1b[34m\nYou are currently in ${directory}\x1b[0m`);
+};
 
 export default inputHandler;
