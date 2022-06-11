071dad5879f9d9d3ef42414d90d01ce8e58c4ebb
justgrace <ivan.koliada69@gmail.com>
2022-06-12 00:27:51 +0300

    feat: add constants

---
+++ src/modules/constants.js
@@ -0,0 +1,25 @@
+const COMMANDS = {
+  UP: 'up',
+  CD: 'cd',
+  LS: 'ls',
+  HASH: 'hash',
+  CAT: 'cat',
+  ADD: 'add',
+  RN: 'rn',
+  CP: 'cp',
+  MV: 'mv',
+  RM: 'rm',
+  COMPRESS: 'compress',
+  DECOMPRESS: 'decompress',
+  OS: 'os'
+}
+
+const OS = {
+  EOL: '--EOL',
+  CPUS: '--cpus',
+  HOME_DIR: '--homedir',
+  USER_NAME: '--username',
+  ARCH: '--architecture',
+};
+
+export { COMMANDS, OS };
